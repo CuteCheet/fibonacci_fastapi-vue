47ee7e0603561596694cb2615a512f64d9e6588e
CuteCheet <135275685+CuteCheet@users.noreply.github.com>
2024-04-07 22:22:20 -0400

    #docs : add comments

--- backend/app/app.py
+++ backend/app/app.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"], # Adjust this to match your Vue.js app's origin
+    allow_origins=["http://127.0.0.1:8080"], # Adjust this to match your Vue.js app's origin
     allow_credentials=True,
     allow_methods=["*"], # Allows all methods
     allow_headers=["*"], # Allows all headers
@@ -14,9 +14,21 @@ app.add_middleware(
 
 @app.get("/fibonacci/{n}")
 async def fibonacci(n: int):
+    """
+    This function calculates the nth number in the Fibonacci sequence.
+    The Fibonacci sequence is a series of numbers in which each number is the sum of the two preceding ones, usually starting with 1 and 1.
+    """
 
-    x, y = 0, 1
+    # Initialize the first two numbers in the Fibonacci sequence
+    first_number, second_number = 0, 1
+
+    # Loop n times to calculate the nth Fibonacci number
     for _ in range(n):
-        x, y = y, x + y
-    res = str(x)
-    return {"fibonacci": res}
+        # Calculate the next number in the sequence by adding the current two numbers
+        first_number, second_number = second_number, first_number + second_number
+
+    # Convert the result to a string for JSON serialization
+    result = str(first_number)
+
+    # Return the result as a JSON object
+    return {"fibonacci": result}
--- frontend/src/components/FibonacciInput.vue
+++ frontend/src/components/FibonacciInput.vue
@@ -1,59 +1,69 @@
 <template>
- <div class="container">
-   <div class="mainFibo_section">
-      <div class="header">
+   <div class="container">
+     <div class="mainFibo_section">
+       <!-- Header section -->
+       <div class="header">
          <h2>Get N<sup>th</sup> Fibonacci Sequence Number</h2>
-      </div>
-      <input v-model="number" type="number" placeholder="Enter a number">
-      <button @click="getFibonacci">Get Number</button>
-      <div class="result">
+       </div>
+       
+       <!-- Input section -->
+       <input v-model="number" type="number" placeholder="Enter a number">
+       <button @click="getFibonacci">Get Number</button>
+       
+       <!-- Result section -->
+       <div class="result">
          <textarea v-model="result" placeholder="The Nth sequence number is..."></textarea>
-      </div>
+       </div>
+     </div>
    </div>
- </div>
-</template>
-
-
-<script>
-import axios from 'axios';
-
-export default {
- data() {
-    return {
-      number: null,
-      result: null,
-    };
- },
- methods: {
-    async getFibonacci() {
-      const url = 'http://localhost:8000/fibonacci/' + this.number;
-      axios.get(url)
-      .then(response => {
-        console.log(response.data);
-        this.result = response.data.fibonacci
-      })
-      .catch(error => {
-        console.error(error);
-      });
-    },
- },
-};
-</script>
-
-<style scoped>
-input, button, textarea {
+ </template>
+ 
+ 
+ <script>
+ import axios from 'axios';
+ 
+ export default {
+   data() {
+     return {
+       number: null, // Holds the input number
+       result: null, // Holds the result of the Fibonacci sequence
+     };
+   },
+   methods: {
+     async getFibonacci() {
+      // Constructs the URL for the API request
+       const url = 'http://localhost:8000/fibonacci/' + this.number; 
+       axios.get(url)
+         .then(response => {
+           console.log(response.data);
+           // Stores the result in the 'result' variable
+           this.result = response.data.fibonacci; 
+         })
+         .catch(error => {
+           console.error(error);
+         });
+     },
+   },
+ };
+ </script>
+ 
+ <style scoped>
+ input, button, textarea {
    font-size: 20px;
-}
-.mainFibo_section {
+ }
+ 
+ .mainFibo_section {
    margin-left: 100px;
    margin-right: 100px;
    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-}
-.result {
+ }
+ 
+ .result {
    padding: 20px;
-}
-.result textarea {
+ }
+ 
+ .result textarea {
    width: 80%;
    height: 200px;
-}
-</style>
+ }
+ </style>
